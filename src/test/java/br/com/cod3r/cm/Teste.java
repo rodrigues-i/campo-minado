f6d3eb60ee9beab160c09763023ac89a8a84fd90
rodrigues-i <lucas-f250@live.com>
2022-06-21 09:18:52 -0300

    Create the classes Campo and Tabuleiro. Then make the basic structure of the class Campo

---
+++ src/test/java/br/com/cod3r/cm/Teste.java
@@ -0,0 +1,20 @@
+package br.com.cod3r.cm;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Teste {
+	
+	@Test
+	void justAnExample() {
+	    int a = 2;
+
+	    assertEquals(2, a);
+	}
+
+	@Test
+	void anotherExample() {
+		int x = 3;
+		assertEquals(3, x);
+	}
+}
